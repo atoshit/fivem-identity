51c62e1d9a28e78d60ec1b6a148365963144057c
atoshit <95099733+atoshit@users.noreply.github.com>
2024-08-15 19:57:54 +0200

    Update client.lua

--- ato-identity/client.lua
+++ ato-identity/client.lua
@@ -2,6 +2,3 @@ AddEventHandler('playerSpawned', function ()
     TriggerServerEvent('ato::RegisterPlayer')
 end)
 
-RegisterCommand('addarmour', function (source, args, raw)
-    SetPedArmour(PlayerPedId(), 100)
-end)
